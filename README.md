0f49ff223a3815a56dc586c946b9d4767073acee
viitones <vitonesmartins@gmail.com>
2024-10-30 07:58:52 -0300

    config extension for pages

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 npx create-next-app@14
 npm i @ignite-ui/react@latest
+npm i @rocketseat/eslint-config -D
--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,9 +1,9 @@
-import {Heading} from '@ignite-ui/react'
+import { Heading } from '@ignite-ui/react'
 
 export default function Home() {
   return (
     <div>
       <Heading as="h1">hello</Heading>
     </div>
-  );
+  )
 }
