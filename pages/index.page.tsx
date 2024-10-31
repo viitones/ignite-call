f934d97fa94f1aa6ff056562a5c9b2b6fe21531a
viitones <vitonesmartins@gmail.com>
2024-10-31 07:50:58 -0300

    estrutura da home

--- pages/index.page.tsx
+++
@@ -1,9 +0,0 @@
-import { Heading } from '@ignite-ui/react'
-
-export default function Home() {
-  return (
-    <div>
-      <Heading as="h1">hello</Heading>
-    </div>
-  )
-}
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,29 @@
+import { Heading, styled, Text } from '@ignite-ui/react'
+
+export const Container = styled('div', {
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
+  marginLeft: 'auto',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$20 ',
+  height: '100vh',
+})
+export const Hero = styled('div', {
+  maxWidth: 480,
+  padding: '0 $10',
+
+  [`${Heading}`]: {},
+
+  [`${Text}`]: {
+    marginTop: '$2',
+    color: '$gray200',
+  },
+})
+export const Preview = styled('div', {
+  paddingRight: '$8',
+  overflow: 'hidden',
+
+  '@media (max-width: 600px)': {
+    display: 'none',
+  },
+})
